5dca2e6bd033c8695f60958ff8ed6f95e808f399
Dev Rajpal <273032235+Bisleri-ji@users.noreply.github.com>
2026-04-04 17:46:17 +0530

    Bisleri Git Push_01
    Added Scene Bisleri_workspace, Menu and some external assets for water.
    Player controller that move the player in 3 lanes like subway surfers.
    Main menu that shifts camera when plays is pressed and the menu hides.

---
+++ Assets/Scripts/Bisleri/PlayerController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
+using System.Collections.Generic;
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float moveSpeed,jump;
+    private Vector2 moveInput;
+    public LayerMask groundLayer;
+    private bool isGrounded;
+    public Transform groundCheck;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        moveInput.x = Keyboard.current.aKey.isPressed ? -1f : (Keyboard.current.dKey.isPressed ? 1f : 0f);
+        moveInput.y = Keyboard.current.sKey.isPressed ? -1f : (Keyboard.current.wKey.isPressed ? 1f : 0f);
+        moveInput.Normalize();
+        rb.linearVelocity = new Vector3(moveInput.x * moveSpeed, rb.linearVelocity.y, moveInput.y * moveSpeed);
+        RaycastHit hit;
+        if(Physics.Raycast(groundCheck.position, Vector3.down, out hit, 0.3f, groundLayer))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        if(Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded)
+        {
+            rb.linearVelocity += (Vector3.up * jump);
+        }
+    }
+}
